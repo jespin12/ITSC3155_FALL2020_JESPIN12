afbad1582a25e87bb2bd85caab702b96915cb961
Jesse Espinoza <jespin12@uncc.edu>
2020-10-25 19:58:12 -0400

    “PythonBasics4 – Part A”

--- PythonBasics4/pythonBasics4.py
+++ PythonBasics4/pythonBasics4.py
@@ -7,14 +7,25 @@
 # # Part A.
 def array_2_dict(emails, contacts):
     # YOUR CODE HERE
+    contactName = list(contacts)
+    for x in range(len(emails)):
+        contacts[contactName[x]] = emails[x]
+    return contacts 
 
-    return
+    
 
 # # Part B.
 def array2d_2_dict(contact_info, contacts):
     # YOUR CODE HERE
+    if len(contact_info) == 0:
+        return contacts
+    else:
+        y = 0
+        x = 0
 
-    return
+        for x in contacts.keys():
+            contacts[x] = contact_info[[y][a]]
+    return contacts
 
 # # Part C.
 def dict_2_array(contacts):
