c4036bb9bc1506a994684d4f484941392da689fe
Jesse Espinoza <jespin12@uncc.edu>
2020-11-14 17:42:25 -0500

    flask app #4 - added db connection to apps

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -6,7 +6,15 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for 
+from database import db
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 
 app = Flask(__name__)     # create an app
 
