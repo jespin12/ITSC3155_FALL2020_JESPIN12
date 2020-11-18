aaf287248817c7fca1c28f874042a9582473df1e
Jesse Espinoza <jespin12@uncc.edu>
2020-11-18 11:56:55 -0500

    flask app #4 - updated queries to return one

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -32,13 +32,13 @@ notes =     {1: {'title': 'First note', 'text': 'This is my first note', 'date':
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu').one()
 
     return render_template ("index.html" , user = a_user)
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu').one()
 
     my_notes = db.session.query(Note).all()
 
@@ -46,9 +46,9 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu').one()
 
-    my_note=db.session.query(Note).filter_by(id=note_id)
+    my_note=db.session.query(Note).filter_by(id=note_id).one()
     
     return render_template ("note.html", note=my_note, user=a_user)
 @app.route('/notes/new', methods=['GET', 'POST'])
@@ -69,7 +69,7 @@ def new_note():
         return redirect(url_for('get_notes'))
 
     else: 
-        a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu')
+        a_user = db.session.query(User).filter_by(email='jespin12@uncc.edu').one()
         return render_template('new.html', user=a_user)
 
 
